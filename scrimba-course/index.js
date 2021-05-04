8169f1f3fd450046d35204c9dbc40d4b07e27091
Theo-Leveque <70675996+Theo-Leveque@users.noreply.github.com>
2021-05-04 11:48:11 -0700

    deleted scrimba course by create react app and creating one from scratch

---
+++ scrimba-course/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+ReactDOM.render(
+  <ul>
+    <li>Ski</li>
+    <li>Run</li>
+    <li>Mountains</li>
+  </ul>,
+  document.getElementById('root')
+);
