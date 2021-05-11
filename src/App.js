a52782893d204f9b1e5c2d86644901010781eadd
Theo-Leveque <70675996+Theo-Leveque@users.noreply.github.com>
2021-05-11 12:30:56 -0700

    covered react router basics: browser router, link, switch and routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,31 @@
-import './App.css';
 import React from 'react';
-import useTypingGame from './components/5.Hooks/CustomHooks/useTypingGame';
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './components/6.ReactRouter/BasicExample/Home';
+import About from './components/6.ReactRouter/BasicExample/About';
+import Contact from './components/6.ReactRouter/BasicExample/Contact';
 
-//
-
-const App = () => {
-  const {
-    text,
-    timer,
-    isGameRunning,
-    wordCount,
-    textRef,
-    handleChange,
-    startGame,
-  } = useTypingGame();
-
+function App() {
   return (
-    <>
-      <h1>Best typing game ever!</h1>
-      <textarea
-        name='game'
-        value={text}
-        cols='30'
-        rows='10'
-        onChange={handleChange}
-        disabled={!isGameRunning}
-        ref={textRef}
-      />
-      <h4>Time remaining: {timer}</h4>
-      <button disabled={isGameRunning} onClick={startGame}>
-        Start!
-      </button>
-      <h2>Number of words: {wordCount}</h2>
-    </>
+    <div className='App'>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
+    </div>
   );
-};
+}
 
 export default App;
-
-// useEffect hook is considered a remplacement for 3 lifecycle methods:
-// componentDidMount
-// componentDidUpdate
-// componentWillUnmount
-// Above all it's a hook that allows us to apply side effect:
-// network request,
-// manual DOM manipulation,
-// event listeners,
-// or timeouts and intervals
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './styles.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
