17948295edd15b8a775a592cb0759334316b2364
daawascript <70675996+daawascript@users.noreply.github.com>
2021-05-20 17:13:26 -0700

    useState and useEffect with fetch

--- adopt-me/src/1.NoFrillsReact/App.js
+++ adopt-me/src/1.NoFrillsReact/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, props.name),
-    React.createElement('h3', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement('div', {}, [
--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(Pet, {
-      name: 'Sudo',
-      animal: 'Dog',
-      breed: 'Wheaten Terrier',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
